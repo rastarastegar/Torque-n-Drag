65679d74cafaa03073a439d5c2abd7eb08c0379b
Leo K <leonardkndichu@gmail.com>
2018-10-06 12:05:08 -0500

    Up to date with master (LandingPage commit), implimenting 2D and 3D. Also point.

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,13 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Sign from "./components/Sign";
 import Login from "./components/Login";
+import Graph2D from  "./components/Graph2D"; 
 import "./App.css";
 
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    ); 
-=======
 
       <div>
         <BrowserRouter>
@@ -33,7 +22,8 @@ class App extends Component {
                <Route exact path="/" component={Main} />
                <Route exact path="/login" component={Login} />
                <Route exact path="/sign-up" component={Sign} />
-             
+               <Route exact path="/graph-2D" component={Graph2D} />
+
             <Footer/>
 
            </div> 
@@ -41,7 +31,6 @@ class App extends Component {
      </div>
 
     );
->>>>>>> aebc51ea6f51abf26913cef1a3c7aaf0907db3e5
   }
 }
 
