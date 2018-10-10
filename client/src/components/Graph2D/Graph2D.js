2380971108d618c0fc42e8dc3728d96b8eebce6e
Alex Federovitch <alexfederovitch@yahoo.com>
2018-10-09 20:46:36 -0500

    adding 3D modeling to the boilerplate

---
+++ client/src/components/Graph2D/Graph2D.js
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+import "./Graph2D.css";
+
+
+class Graph2D extends Component {
+
+    componentDidMount() {
+        var chart = new CanvasJS.Chart("chartContainer", {
+            title:{
+                text:"Fortune Global 500 Companies by Country"
+            },
+            animationEnabled: true,
+           
+            data: [
+            {     
+                type: "bar",
+                dataPoints: [
+                  {y: 5, label: "Sweden"  },
+                  {y: 6, label: "Taiwan"  },
+                  {y: 7, label: "Russia"  },
+                  {y: 8, label: "Spain"  },
+                  {y: 8, label: "Brazil"  },
+                  {y: 8, label: "India"  },
+                  {y: 9, label: "Italy"  },
+                  {y: 9, label: "Australia"  },
+                  {y: 12, label: "Canada"  },
+                  {y: 13, label: "South Korea"  },
+                  {y: 13, label: "Netherlands"  },
+                  {y: 15, label: "Switzerland"  },
+                  {y: 28, label: "Britain" },
+                  {y: 32, label: "Germany"   },
+                  {y: 32, label: "France"  },
+                  {y: 68, label: "Japan"   },
+                  {y: 73, label: "China"},
+                  {y: 132, label: "US" }
+                ]
+            }
+
+            ]
+        });
+    chart.render();
+  }
+  render() {
+    return (
+      <div id="chartContainer" style={{height: 450 + "px", width: 100 + "%"}}>
+      </div>
+    );
+  }
+
+}
+    
+export default Graph2D;
---
+++ client/src/components/Graph3D/Graph3D.js
@@ -0,0 +1,77 @@
+import "./3.js";
+import React, {Component} from "react";
+import "./Graph3D.css";
+//import { Link } from 'react-router-dom'
+import CsvParse from  "../UploadCSV"; 
+import "./tubular.js";
+import "./vectors.js";
+import * as THREE from "three";
+import ExpoTHREE from "expo-three";
+
+
+class Graph3D extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            data: null,
+            error: null,
+        }
+        }
+
+        handleData = data => {
+        this.setState({ data })
+        }
+
+        handleError = error => {
+        this.setState({ error })
+        }
+
+        render() {
+        const keys = [
+            'Depth (ft)',
+            'Incl. (Deg.)',
+            'Azim. (Deg.)',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+            'XXXX',
+        ]
+
+        return (
+            <div>
+            <h1>Demo React Csv Parse</h1>
+
+            <CsvParse
+                keys={keys}
+                onDataUploaded={this.handleData}
+                onError={this.handleError}
+                render={onChange => <input type="file" onChange={onChange} />}
+            />
+
+            
+
+
+
+
+
+            {this.state.data && (
+                <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+            )}
+
+            {this.state.error && (
+                <pre>{JSON.stringify(this.state.error, null, 2)}</pre>
+            )}
+            </div>
+        )
+    }
+}
+
+
+export default Graph3D;
