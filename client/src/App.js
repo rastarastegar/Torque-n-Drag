12084471588e8ecafe8ca6707890c436ee35d0e2
Wilson P <wilson.prevette@gmail.com>
2018-10-13 13:44:27 -0500

    deleted extra css files

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from "./components/LoadingPage.js";
