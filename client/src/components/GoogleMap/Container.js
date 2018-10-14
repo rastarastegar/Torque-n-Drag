246a5923789fdce2a6adcef6778325d0afca58cf
Wilson P <wilson.prevette@gmail.com>
2018-10-13 19:23:43 -0500

    changes to container comp

--- client/src/components/GoogleMap/Container.js
+++ client/src/components/GoogleMap/Container.js
@@ -12,3 +12,18 @@ export class Container extends React.Component {
   export default GoogleApiComponent({
     apiKey: "AIzaSyD1KLKK9vZ_Cs7AAlQFDEJlSa1X0xvq9tM"
   })(Container)
+
+  export class Container extends React.Component {
+    render() {
+      const style = {
+        width: '100vw',
+        height: '100vh'
+      }
+      return (
+        <div style={style}>
+          <Map google={this.props.google}
+            />
+        </div>
+      )
+    }
+  }
