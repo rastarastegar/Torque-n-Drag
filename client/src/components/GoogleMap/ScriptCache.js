0abe3473bfcb8f78fc80b435fae295307bb8497e
Wilson P <wilson.prevette@gmail.com>
2018-10-13 19:17:40 -0500

    added rest of scriptcache

--- client/src/components/GoogleMap/ScriptCache.js
+++ client/src/components/GoogleMap/ScriptCache.js
@@ -17,3 +17,36 @@ export const ScriptCache = (function(global) {
         }
       }
     }
+
+    Cache._scriptTag = (key, src) => {
+        if (!scriptMap.has(key)) {
+          let tag = document.createElement('script');
+          let promise = new Promise((resolve, reject) => {
+            let resolved = false,
+                errored = false,
+                body = document.getElementsByTagName('body')[0];
+  
+            tag.type = 'text/javascript';
+            tag.async = false; // Load in order
+  
+            const cbName = `loaderCB${counter++}${Date.now()}`;
+            let cb;
+  
+            let handleResult = (state) => {
+              return (evt) => {
+                let stored = scriptMap.get(key);
+                if (state === 'loaded') {
+                  stored.resolved = true;
+                  resolve(src);
+                  // stored.handlers.forEach(h => h.call(null, stored))
+                  // stored.handlers = []
+                } else if (state === 'error') {
+                  stored.errored = true;
+                  // stored.handlers.forEach(h => h.call(null, stored))
+                  // stored.handlers = [];
+                  reject(evt)
+                }
+  
+                cleanup();
+              }
+            }
