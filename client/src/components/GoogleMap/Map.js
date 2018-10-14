8c7ea53b7492db36c9fb35a670bcdf59471d12b8
Wilson P <wilson.prevette@gmail.com>
2018-10-13 19:25:30 -0500

    added test map comp

---
+++ client/src/components/GoogleMap/Map.js
@@ -0,0 +1,23 @@
+export class Map extends React.Component {
+    loadMap() {
+      if (this.props && this.props.google) {
+        // google is available
+        const {google} = this.props;
+        const maps = google.maps;
+  
+        const mapRef = this.refs.map;
+        const node = ReactDOM.findDOMNode(mapRef);
+  
+        let zoom = 14;
+        let lat = 37.774929;
+        let lng = -122.419416;
+        const center = new maps.LatLng(lat, lng);
+        const mapConfig = Object.assign({}, {
+          center: center,
+          zoom: zoom
+        })
+        this.map = new maps.Map(node, mapConfig);
+      }
+      // ...
+    }
+  }
